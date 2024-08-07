9025a2b73241eb6632a7b41812ff7e1184f9842a
Aymane <aymantt2004@gmail.com>
2024-08-07 13:59:12 +0100

    create a task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTaskRequest;
 
 class TaskController extends Controller
 {
@@ -19,5 +20,12 @@ class TaskController extends Controller
         }
         return response()->json($task);
     }
+
+    public function createTask(CreateTaskRequest $request){
+        Task::create($request->validated());
+
+        return response()->json(['message' => 'Task created with success !'],201);
+    }
+
 }
 
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::get('/todo/task',[TaskController::class,'getTasks']);
+
+Route::get('/todo/task/{id}',[TaskController::class,'getTaskById']);
+
+Route::post('/todo/task',[TaskController::class,'createTask']);
